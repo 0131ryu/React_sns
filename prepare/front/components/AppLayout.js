1e6571d52bd7b7dc97b168ca6fba34a55f46fbe3
0131ryu <131ryuji@gmail.com>
2022-10-04 13:51:04 +0900

    반응형 그리드 사용

--- prepare/front/components/AppLayout.js
+++ prepare/front/components/AppLayout.js
@@ -1,22 +1,50 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Link from "next/link";
+import { Menu, Input, Row, Col, Button } from "antd";
+const { Search } = Input;
+
+const onSearch = (value) => console.log(value);
+
+const items = [
+  { label: <Link href="/">노드버드</Link>, key: "item-1" },
+  { label: <Link href="/profile">프로필</Link>, key: "item-2" },
+  {
+    label: (
+      <Search
+        placeholder="검색하기"
+        onSearch={onSearch}
+        style={{ verticalAlign: "middle" }}
+      />
+    ),
+    key: "item-3",
+  },
+  { label: <Link href="/signup">회원가입</Link>, key: "item-4" },
+];
 
 const AppLayout = ({ children }) => {
   return (
     <div>
-      <div>
-        <Link href={"/"}>
-          <a>노드버드</a>
-        </Link>
-        <Link href={"/profile"}>
-          <a>프로필</a>
-        </Link>
-        <Link href={"/signup"}>
-          <a>회원가입</a>
-        </Link>
-      </div>
+      <Menu items={items} mode="horizontal" />
       {children}
+      <Row gutter={8}>
+        <Col xs={24} md={6}>
+          왼쪽 메뉴
+        </Col>
+        <Col xs={24} md={12}>
+          테스트
+        </Col>
+        <Col xs={24} md={6}>
+          <Button
+            type="link"
+            href="https://ba-gotocode131.tistory.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            블로그
+          </Button>
+        </Col>
+      </Row>
     </div>
   );
 };
--- prepare/front/pages/profile.js
+++ prepare/front/pages/profile.js
@@ -1,5 +1,17 @@
+import Head from "next/head";
+import AppLayout from "../components/AppLayout";
+
 const Profile = () => {
-  return <div>프로필 페이지</div>;
+  return (
+    <>
+      <Head>
+        <title>내 프로필 | NodeBird </title>
+      </Head>
+      <AppLayout>
+        <div>프로필 페이지</div>
+      </AppLayout>
+    </>
+  );
 };
 
 export default Profile;
