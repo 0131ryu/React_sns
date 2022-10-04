dec8585351da8656ca7290dab184334e0f1c79e1
0131ryu <131ryuji@gmail.com>
2022-10-04 17:19:10 +0900

    더미데이터로 로그인, 프로필 페이지 만들기

--- prepare/front/components/AppLayout.js
+++ prepare/front/components/AppLayout.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import Link from "next/link";
+import styled from "styled-components";
 import { Menu, Input, Row, Col, Button } from "antd";
 const { Search } = Input;
+import UserProfile from "./UserProfile";
+import LoginForm from "./LoginForm";
+
+const SearchInput = styled(Search)`
+  vertical-align: "middle";
+`;
 
 const onSearch = (value) => console.log(value);
 
@@ -10,25 +17,25 @@ const items = [
   { label: <Link href="/">노드버드</Link>, key: "item-1" },
   { label: <Link href="/profile">프로필</Link>, key: "item-2" },
   {
-    label: (
-      <Search
-        placeholder="검색하기"
-        onSearch={onSearch}
-        style={{ verticalAlign: "middle" }}
-      />
-    ),
+    label: <SearchInput placeholder="검색하기" onSearch={onSearch} />,
     key: "item-3",
   },
   { label: <Link href="/signup">회원가입</Link>, key: "item-4" },
 ];
 
 const AppLayout = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <div>
       <Menu items={items} mode="horizontal" />
       {children}
       <Row gutter={8}>
         <Col xs={24} md={6}>
+          {isLoggedIn ? (
+            <UserProfile setIsLoggedIn={setIsLoggedIn} />
+          ) : (
+            <LoginForm setIsLoggedIn={setIsLoggedIn} />
+          )}
           왼쪽 메뉴
         </Col>
         <Col xs={24} md={12}>
