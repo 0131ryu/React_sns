4dab8f6f7ef5d8513a7b869e99d5ff5f0df36b9c
0131ryu <131ryuji@gmail.com>
2022-10-18 16:46:57 +0900

    cutom.css 작성 및 HomeView에서 ref, 함수들 사용

--- quiz_vue/src/views/HomeView.vue
+++ quiz_vue/src/views/HomeView.vue
@@ -27,65 +27,39 @@
           <!-- question container -->
 
           <div class="bg-white p-5">
-            Who is the most powerful Avenger in the marvel cinematic universe?>
+            {{ currentQuestion.question }}
           </div>
         </div>
 
         <!-- options container -->
         <div class="mt-8">
-          <!-- option container -->
-          <div
-            class="neumorph-1 option-default bg-gray-100 p-2 rounded-lg mb-3 relative"
-          >
-            <div class="bg-white rounded-lg font-bold flex p-2">
-              <!-- option ID -->
-              <div class="bg-gray-400 p-3 rounded-lg">A</div>
-              <!-- option name -->
-              <div class="flex items-center pl-6">Thor</div>
-            </div>
-          </div>
-
-          <!-- option container -->
-          <div class="neumorph-1 bg-gray-100 p-2 rounded-lg mb-3">
-            <div class="bg-red-600 rounded-lg font-bold flex p-2 text-white">
-              <!-- option ID -->
-              <div class="bg-gray-800 p-3 rounded-lg">B</div>
-              <!-- option name -->
-              <div class="flex items-center pl-6">Doctor Strange</div>
-            </div>
-          </div>
-
-          <!-- option container -->
-          <div class="neumorph-1 bg-gray-100 p-2 rounded-lg mb-3 relative">
+          <div v-for="(choice, item) in currentQuestion.choices" :key="item">
+            <!-- option container -->
             <div
-              class="bg-blue-700 p-1 transform rotate-45 rounded-md h-10 w-10 text-white font-bold absolute right-0 top-0 shadow-md"
+              class="neumorph-1 option-default bg-gray-100 p-2 rounded-lg mb-3 relative"
+              :ref="optionChosen"
+              @click="onOptionClicked(choice, item)"
             >
-              <div class="transform -rotate-45">+10</div>
-            </div>
-            <div class="bg-green-700 rounded-lg font-bold flex p-2 text-white">
-              <!-- option ID -->
-              <div class="bg-gray-800 p-3 rounded-lg">C</div>
-              <!-- option name -->
-              <div class="flex items-center pl-6">ms Marvel</div>
-            </div>
-          </div>
+              <div
+                class="bg-blue-700 p-1 transform rotate-45 rounded-md h-10 w-10 text-white font-bold absolute right-0 top-0 shadow-md"
+              >
+                <p class="transform -rotate-45">+10</p>
+              </div>
 
-          <!-- option container -->
-          <div class="neumorph-1 bg-gray-100 p-2 rounded-lg mb-3">
-            <div class="bg-white rounded-lg font-bold flex p-2">
-              <!-- option ID -->
-              <div class="bg-gray-400 p-3 rounded-lg">D</div>
-              <!-- option name -->
-              <div class="flex items-center pl-6">Hulk</div>
+              <div class="rounded-lg font-bold flex p-2">
+                <!-- option ID -->
+                <div class="p-3 rounded-lg">{{ item }}</div>
+                <!-- option name -->
+                <div class="flex items-center pl-6">{{ choice }}</div>
+              </div>
             </div>
           </div>
+        </div>
 
-          <!-- progress  indicator container -->
-          <div class="mt-8 text-center">
-            <div class="h-1 w-12 bg-gray-800 rounded-full mx-auto"></div>
-            <p class="font-bold text-gray-800"></p>
-            <p>2/10</p>
-          </div>
+        <!-- progress  indicator container -->
+        <div class="mt-8 text-center">
+          <div class="h-1 w-12 bg-gray-800 rounded-full mx-auto"></div>
+          <p class="font-bold text-gray-800">2/10</p>
         </div>
       </div>
     </div>
@@ -96,8 +70,85 @@
 .neumorph-1 {
   box-shadow: 6px 6px 18px rgba(0, 0, 0, 0.09), -6px -6px 18px #ffffff;
 }
+
 .container {
   max-width: 400px;
   border-radius: 25px;
 }
 </style>
+
+<script>
+import { onMounted, ref } from "vue";
+
+export default {
+  setup() {
+    let questionCounter = ref(0);
+    const currentQuestion = ref({
+      question: "",
+      answer: 1,
+      choices: [],
+    });
+
+    const questions = [
+      {
+        question:
+          "Which programming language shares its name with an island in Indonesia?",
+        answer: 1,
+        choices: ["Java", "Python", "C", "Jakarta"],
+      },
+      {
+        question: "On Twitter, what is the character limit for a Tweet?",
+        answer: 3,
+        choices: ["120", "160", "140", "100"],
+      },
+      {
+        question: "What does the 'MP' stand for in MP3?",
+        answer: 4,
+        choices: [
+          "Music Player",
+          "Multi Pass",
+          "Micro Point",
+          "Moving Picture",
+        ],
+      },
+    ];
+    const onQuizStart = () => {
+      currentQuestion.value = questions[questionCounter.value];
+    };
+
+    let itemsRef = [];
+    const optionChosen = (element) => {
+      if (element) {
+        itemsRef.push(element);
+      }
+    };
+    const onOptionClicked = (choice, item) => {
+      // console.log(itemsRef[item]);
+      const divContainer = itemsRef[item];
+      const optionID = item + 1;
+      if (currentQuestion.value.answer === optionID) {
+        console.log("your are correct");
+        divContainer.classList.add("option-correct");
+        divContainer.classList.remove("option-default");
+      } else {
+        console.log("you are wrong");
+        divContainer.classList.add("option-wrong");
+        divContainer.classList.remove("option-default");
+      }
+      console.log(choice, item);
+    };
+
+    onMounted(() => {
+      onQuizStart();
+    });
+
+    return {
+      currentQuestion,
+      questions,
+      questionCounter,
+      onOptionClicked,
+      optionChosen,
+    };
+  },
+};
+</script>
