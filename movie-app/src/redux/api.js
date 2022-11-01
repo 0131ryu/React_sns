40fff0ef762253ef59d9e3aa4f6ea0b63b2168cc
0131ryu <131ryuji@gmail.com>
2022-11-01 15:02:12 +0900

    redux-toolkit, redux-saga로 고양이 종류 보여주기

--- movie-app/src/redux/api.js
+++ movie-app/src/redux/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
-const API_ENDPOINT = `http://www.omdbapi.com/?apikey=${process.env.MOVIE_API_KEY}`;
+// const API_ENDPOINT = `http://www.omdbapi.com/?apikey=${process.env.MOVIE_API_KEY}`;
 
 export const fetchMovies = async (movieName) => {
-  return axios.get(`${API_ENDPOINT}&s=${movieName}`);
+  return axios.get(
+    `http://www.omdbapi.com/?s=${movieName}&apikey=${process.env.MOVIE_API_KEY}`
+  );
 };
---
+++ toolkit-saga-app/src/App.js
@@ -0,0 +1,46 @@
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getCatsFetch } from "./catState";
+import "./App.css";
+
+function App() {
+  const cats = useSelector((state) => state.cats.cats);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getCatsFetch());
+  }, [dispatch]);
+  console.log(cats);
+  return (
+    <div className="App">
+      <h1>CAT SPECIES GALLERY</h1>
+      <p>
+        Images of different species of cats. Lots of cats for your viewing
+        pleasure.
+      </p>
+      <hr />
+      <div className="Gallery">
+        {cats.map((cat) => (
+          <div key={cat.id} className="row">
+            <div className="colum colum-left">
+              <img
+                alt={cat.name}
+                src={cat.image.url}
+                width="200"
+                height="200"
+              />
+            </div>
+            <div className="colum colum-right">
+              <h2>{cat.name}</h2>
+              <h5>Temperament : {cat.temperament}</h5>
+              <p>{cat.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+      <button>View More Cats</button>
+    </div>
+  );
+}
+
+export default App;
