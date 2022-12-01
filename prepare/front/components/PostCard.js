8297391408efb6be6afaa1b1291790190eb95ebf
0131ryu <131ryuji@gmail.com>
2022-12-01 12:12:48 +0900

    fix: prefetch false

--- prepare/front/components/PostCard.js
+++ prepare/front/components/PostCard.js
@@ -128,7 +128,7 @@ const PostCard = ({ post }) => {
           >
             <Card.Meta
               avatar={
-                <Link href={`/user/${post.Retweet.User.id}`}>
+                <Link href={`/user/${post.Retweet.User.id}`} prefetch={false}>
                   <a>
                     <Avatar>{post.Retweet.User.nickname[0]}</Avatar>
                   </a>
@@ -141,7 +141,7 @@ const PostCard = ({ post }) => {
         ) : (
           <Card.Meta
             avatar={
-              <Link href={`/user/${post.User.id}`}>
+              <Link href={`/user/${post.User.id}`} prefetch={false}>
                 <a>
                   <Avatar>{post.User.nickname[0]}</Avatar>
                 </a>
@@ -164,7 +164,7 @@ const PostCard = ({ post }) => {
                 <Comment
                   author={item.User.nickname}
                   avatar={
-                    <Link href={`/user/${item.User.id}`}>
+                    <Link href={`/user/${item.User.id}`} prefetch={false}>
                       <a>
                         <Avatar>{item.User.nickname[0]}</Avatar>
                       </a>
